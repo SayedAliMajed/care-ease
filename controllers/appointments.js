4db8be69cc8a14c06ce1901e88634a8503a8a810
SayedAli <sayed.ali.majed@gmail.com>
2025-09-26 20:47:08 +0300

    completed partials and updated appointments  routes (view,new,create and show)

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -6,15 +6,16 @@ const User = require('../models/user');
 
 router.get('/', async (req, res) => {
   try {
-    const ownerId = req.session.user._id;
-    const populatedAppointments = await Appointment.find({ owner: ownerId }).populate('owner');
-    res.render('appointments/index.ejs', { appointments: populatedAppointments }); // lowercase key
+    const patientId = req.session.user._id;
+    const appointments = await Appointment.find({ patient_id: patientId }).populate('employee_id patient_id');
+    res.render('appointments/index.ejs', { appointments });
   } catch (error) {
     console.log(error);
     res.redirect('/');
   }
 });
 
+// Appointment creation form
 router.get('/new', async (req,res) => {
   try {
     res.render('appointments/new')
@@ -24,9 +25,37 @@ router.get('/new', async (req,res) => {
     res.redirect('/');
 
   }
-})
+});
 
+// Create new appointment
+router.post('/', async (req,res) => {
+  try {
+    req.body.patientId = req.session.user._id;
+    await Appointment.create(req.body);
+    
+    res.redirect('/appointments');
+  } catch(error) {
+    console.log(error)
+    res.redirect('/');
 
+  }
+});
+
+// Show Page
+
+router.get('/:appointmentId', async (req,res) => {
+  try {
+    const appointments = await Appointment.findById(req.params.appointmentId)
+    .populate('patient_id')
+    .populate('doctorId')
+    res.render('appointments/show.ejs', {
+      appointments
+    });
+  } catch(error) {
+    Console.log(error);
+    res.redirect('/');
+  }
+});
 
 
 
